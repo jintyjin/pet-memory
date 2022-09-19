49d915141c3237bd59c1b169d4632a8c12817280
jin <jintyjin15@gmail.com>
2022-09-19 15:54:04 +0900

    first commit

---
+++ src/main/java/com/family/petmemory/PetMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.family.petmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetMemoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/family/petmemory/PetMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.family.petmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
