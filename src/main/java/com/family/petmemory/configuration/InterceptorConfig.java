987d5b526e0b9c72eb9fd658594ce08859d0634c
jin <jintyjin15@gmail.com>
2022-09-30 17:23:03 +0900

    feat: design pages

---
+++ src/main/java/com/family/petmemory/configuration/InterceptorConfig.java
@@ -0,0 +1,18 @@
+package com.family.petmemory.configuration;
+
+import com.family.petmemory.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/new", "/css/**", "/*.ico", "/error", "/login", "/logout");
+    }
+}
---
+++ src/main/java/com/family/petmemory/interceptor/LoginInterceptor.java
@@ -0,0 +1,26 @@
+package com.family.petmemory.interceptor;
+
+import com.family.petmemory.entity.session.SessionConst;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+
+        HttpSession session = request.getSession();
+
+        if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
+            response.sendRedirect("/login?redirectURL=" + requestURI);
+            return false;
+        }
+
+        return true;
+    }
+}
