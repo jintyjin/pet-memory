93812aaea006b29b164ff79635b40e7977196c52
jin <jintyjin15@gmail.com>
2023-02-22 20:13:50 +0900

    chore: change variable in Gps class

--- src/main/java/com/family/petmemory/entity/memory/Gps.java
+++ src/main/java/com/family/petmemory/entity/memory/Gps.java
@@ -8,14 +8,14 @@ import javax.persistence.Embeddable;
 @Getter
 public class Gps {
 
-    private Double x;
-    private Double y;
+    private Double Latitude;
+    private Double Longitude;
 
     protected Gps() {
     }
 
-    public Gps(Double x, Double y) {
-        this.x = x;
-        this.y = y;
+    public Gps(Double Latitude, Double Longitude) {
+        this.Latitude = Latitude;
+        this.Longitude = Longitude;
     }
 }
